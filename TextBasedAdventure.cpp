f5edf73ae08b0ad5239cd586e3eb30d9614295d0
derbyboy22 <49322739+derbyboy22@users.noreply.github.com>
2021-04-11 23:52:16 -0400

    Find Item functions
    Added functions to find listed items and add them to inventory.

---
+++ TextBasedAdventure.cpp
@@ -0,0 +1,26 @@
+/*
+* Textbased Game Theme Jungle Escape
+* Version:1.0
+* Authors:JiraJocks
+*
+*/
+
+#include "txtGameHeader.h"
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <iomanip>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	mainMenu();
+	playerOne();
+	gameScenarioSelections();
+	//setDamage(30);
+	//getHealth();
+	//setHeal(99);
+	//getHealth();
+	return 0;
+}
